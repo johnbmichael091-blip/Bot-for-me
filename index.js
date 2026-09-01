9ffc9a6f7f7292d937f8a7e00b9673abccb71f2e
johnbmichael091-blip <johnbmichael091@gmail.com>
2026-09-01 14:39:56 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+  "name": "discord-phrase-moderator",
+  "version": "1.0.0",
+  "description": "Discord bot that deletes messages containing configured phrases and times out the author.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "engines": {
+    "node": ">=18"
+  },
+  "dependencies": {
+    "discord.js": "^14.21.0"
+  }
+}
